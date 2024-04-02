58223fe7dcf8db3208a70e23bc3637de7172bb14
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-02 16:42:26 -0700

    test

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
+  mode: 'development',
   entry: './server/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -16,9 +16,39 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
     ],
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-  },
+    fallback: { 
+    "path": require.resolve("path-browserify"),
+    "fs": false,
+    "crypto": require.resolve("crypto-browserify"),
+    "assert": require.resolve("assert"),
+    "process": require.resolve("process"),
+    "url": require.resolve("url"),
+    "stream": require.resolve("stream-browserify"),
+    "os": require.resolve("os-browserify"),
+    "zlib": require.resolve("browserify-zlib"),
+    "tls": require.resolve("tls-browserify"),
+    "net": require.resolve("net-browserify"),
+    "querystring": require.resolve("querystring-es3"),
+    "http": require.resolve("stream-http"),
+    "aws-sdk": require.resolve("aws-sdk"),
+    "nock": require.resolve("nock"),
+    "child_process": false,
+    "mock-aws-s3": require.resolve("mock-aws-s3"),
+    "https": require.resolve("https-browserify"),
+    "timers": require.resolve("timers-browserify"),
+    "constants": require.resolve("constants-browserify"),
+
+  }
+  }
 };
