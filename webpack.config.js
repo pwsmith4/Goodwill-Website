e3c50745799b6b3c442ed36353768bf6e91142f3
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-03 15:34:56 -0700

    Remove node_modules from version control

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = [
         {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
-          include: [path.resolve(__dirname, 'client'), path.resolve(__dirname, 'client/src')],
+          include: [path.resolve(__dirname, 'client'), path.resolve(__dirname, 'src'), path.resolve(__dirname, 'client/src')],
           use: {
             loader: 'babel-loader',
             options: {
