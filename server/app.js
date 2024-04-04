aef629894422e423cc445426d7457c6783aef0c5
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-04 13:51:19 -0700

    test

--- server/app.js
+++ server/app.js
@@ -22,6 +22,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
+});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
