a9ba539654d9a9663f9a1e1f92704fb624fd90a7
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-04 14:10:10 -0700

    hi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,30 @@
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login, Signup} from './pages';
 import React from 'react';
+import Welcome from './pages/Welcome';
+import Home from './pages/Home';
+import Privacy from './pages/Privacy';
+import EditAccount from './pages/EditAccount';
+import UserContext from './context/UserContext';
 
 function App() {
-  return <h1>Hello, world!</h1>;
+  const [user, setUser] = React.useState(null);
+
+  return (
+      <Router>
+        <UserContext.Provider value={{ user, setUser }}>
+        <Routes>
+          <Route path="/welcome" element={<Welcome />} />
+          <Route path="/privacy" element={<Privacy />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/edit-account" element={<EditAccount />} />
+        </Routes>
+        </UserContext.Provider>
+      </Router>
+  );
 }
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 module.exports = [
   {
     mode: 'development',
-    entry: './server/index.jsx',
+    entry: './server/index.js',
     output: {
       path: path.resolve(__dirname, 'dist'),
       filename: 'serverBundle.js',
