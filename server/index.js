ffbd7f3fb390be62196231d633180e18028c737d
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-05 13:53:37 -0700

    hi

--- server/index.js
+++ server/index.js
@@ -8,14 +8,16 @@ const cookieParser = require("cookie-parser");
 const authRoute = require("./Routes/AuthRoute");
 //const { MONGO_URL, PORT } = process.env;
 const PORT = process.env.PORT | 4000;
+
 mongoose
   .connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB is connected successfully"))
-  .catch((err) => console.error("Error: " +err));
-
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
+  .catch((err) => {
+    console.error("Error: " + err);
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  });
 
 app.use(
   cors({
