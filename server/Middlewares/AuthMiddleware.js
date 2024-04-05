c2f24e8a789bf10bde193174d08b4c53eab0418d
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-04 21:59:59 -0700

    hi

--- server/Middlewares/AuthMiddleware.js
+++ server/Middlewares/AuthMiddleware.js
@@ -5,15 +5,15 @@ const jwt = require("jsonwebtoken");
 module.exports.userVerification = (req, res) => {
   const token = req.cookies.token
   if (!token) {
-    return res.json({ status: false })
+    return res.json({ status: falsee })
   }
   jwt.verify(token, process.env.TOKEN_KEY, async (err, data) => {
     if (err) {
-     return res.json({ status: false })
+     return res.json({ status: falsef })
     } else {
       const user = await User.findById(data.id)
       if (user) return res.json({ status: true, user })
-      else return res.json({ status: false })
+      else return res.json({ status: falseg })
     }
   })
 }
