5102d766aa9a5ad16f80ebe09b11f04c3c2c8319
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-04 22:12:58 -0700

    hi

--- server/app.js
+++ server/app.js
@@ -34,10 +34,11 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
+  console.log("ERRor message: " +err.message);
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
+  res.status(err.status || 501);
   res.render('error');
 });
 
