4be230c43f90ec04cefacc48f1541f1a89f3a66b
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-08 17:17:48 -0700

    hi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -275,15 +275,15 @@ const Home = () => {
     <div className="modal-content1" onClick={e => e.stopPropagation()}>
       <button className="close-button1" onClick={() => setIsCashDonationModalOpen(false)}>X</button>
       <h2 style={{ textAlign: 'center' }}>Cash Donation</h2>
-      <div className="input-group" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+      <div className="input-group" style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
   <label htmlFor="cashAmountInput" style={{ marginRight: '10px' }}>Cash Amount:</label>    
   <input
     id="cashAmountInput"
     value={cashAmountInput}
     onChange={e => setCashAmountInput(e.target.value)}
   />
-  <button className="yellow-modal-button" onClick={handleCashDonationSubmit}>Submit</button>
-</div>
+  </div>
+  <button className="yellow-modal-button" onClick={handleCashDonationSubmit} style={{ marginTop: '50px' }}>Submit</button>
     </div>
   </div>
 )}
