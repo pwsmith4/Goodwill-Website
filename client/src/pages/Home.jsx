e4a6b164f321421353265e520fd9b0167a6e4d43
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-10 14:19:51 -0700

    hi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -57,6 +57,7 @@ const Home = () => {
     }, []);
 
     const handleModalSubmit = async () => {
+      
       try {
         console.log("Receipt ID Input: ", receiptIdInput);
         const { data } = await axios.get(
