2e5ecfcd005d43497398b250e95f61e2e8672c06
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-10 14:05:47 -0700

    hi

--- server/Routes/AuthRoute.js
+++ server/Routes/AuthRoute.js
@@ -88,7 +88,7 @@ router.put('/users/:id', async (req, res) => {
   try {
     const { newReceipt } = req.body;
     // Check if newReceipt is provided
-    /*
+    
     if (!newReceipt) {
       return res.status(400).send('No receipt provided');
     }
@@ -97,7 +97,7 @@ router.put('/users/:id', async (req, res) => {
     if (!user) {
       return res.status(404).send('User not found');
     }
-    console.log("User before server: ", user);*/
+    console.log("User before server: ", user);
 /*  // Separate the info in newReceipt
   const { receipt_id, timestamp, store_number, donation_value } = newReceipt;
 
