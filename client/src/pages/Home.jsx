328200708172b06fdd87115b785b93d529d821e4
pwsmith4 <97992667+pwsmith4@users.noreply.github.com>
2024-04-14 12:23:57 -0700

    hi

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -88,7 +88,7 @@ const Home = () => {
           console.log(`Sending to axios: ${process.env.REACT_APP_BASE_URL}/users/${user.user._id}`);
           console.log("User id: ", user.user._id);
           console.log("User Receipts: ", user.user.user_receipts);
-          setReceipts(user.user_receipts);
+          setReceipts(user.user.user_receipts);
 
           console.log("User Receipts Updated in database");
           setIsModalOpen(false);
